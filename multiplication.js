c114963f665de25813f13c1557b599b553cab721
Aurelie <brizouaurelie@gmail.com>
2018-06-06 19:47:41 +0200

    Ajout d'une boucle et saisie utilisateur à multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -1,1 +1,6 @@
 // table de multiplication 
+
+var nombre = 0; // Valeur par défaut permettant d'entrer dans la boucle
+while ((nombre < 2) || (nombre > 9)) {
+    nombre = Number(prompt("Entrez un nombre entre 2 et 9 : "));
+}
